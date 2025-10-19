ecc2954ea7982a3c74e6c8f6a4b7fd0db04d277c
Alejandro Gándara <alejandro.gandara.a@gmail.com>
2025-10-19 13:28:19 -0600

    Seller class added

--- classes/Seller.py
+++ classes/Seller.py
@@ -1,12 +1,76 @@
 """This module defins the Seller class."""
-    @property
-    def services_offered(self, services_offered: list) -> list:
-        """The services offered by the seller.
-        
+
+from classes.ServicesManager import ServicesManager
+
+
+class Seller:
+    """This class is responsible for managing the information of a sale,
+    such as the list of services sold and the total.
+
+    Attributes:
+        services_sold (list): list of services in a sale.
+        total (float): total amount of the sale.
+        services_offered (ServicesManager): instance of ServicesManager class.
+
+    Methods:
+        add_service: Adds a service to the services_sold list.
+        remove_service: Removes a service from the services_sold list.
+        calculate_total: Calculates the total amount of the sale.
+    """
+
+    def __init__(
+        self, total_price: float, services_offered: ServicesManager, services_sold: list
+    ):
+        """Initialize the Seller class.
+
         Arguments:
-            services_offered (list): list of services offered by the seller.
-            
-        Returns: 
-            list: list of names of services offered by the seller."""
-            
-        return [service.name for service in services_offered]
+            services_sold (list): list of services in a sale.
+            total (float): total amount of the sale.
+            services_offered (ServicesManager): instance of ServicesManager class.
+        """
+        self.services_sold = services_sold
+        self.total_price = total_price
+        self.services_offered = ServicesManager(services_offered)
+
+    def add_service(self, service_added: str) -> None:
+        """Adds a service to the services_sold list
+
+        Arguments:
+            service_added (str): name of the service to be added.
+
+        Exeptions:
+            ValueError: If the service is not found in the services_sold list.
+        """
+        for service in self.services_offered.services_offered:
+            if service.name == service_added:
+                self.services_sold.append(service)
+                break
+            else:
+                raise ValueError("El servicio o producto no existe")
+
+    def remove_service(self, service_removed: str) -> None:
+        """Removes a service from the services_sold list.
+
+        Arguments:
+            service_removed (str): name of the service to be removed.
+
+        Exeptions:
+            ValueError: If the service is not found in the services_sold list."""
+        for service in self.services_sold:
+            if service.name == service_removed:
+                self.services_sold.remove(service)
+                break
+            else:
+                raise ValueError(
+                    "El servicio o producto no se encuentra en la lista de venta"
+                )
+
+    def calculate_total(self) -> float:
+        """Calculates the toatl amoun of the sale.
+
+        returns:
+            float: total amount of the sale.
+        """
+        for service in self.services_sold:
+            self.total_price += service.price
+        return self.total_price
--- classes/ServicesManager.py
+++ classes/ServicesManager.py
@@ -3,12 +3,12 @@
 from classes.Service import Service
 
 
-class ServicesManager():
+class ServicesManager:
     """This class is in charge of managing the services offered by the seller.
-    
+
     Atributes:
         services_offered (list): list of services offered by the seller.
-    
+
     Methods:
         add_service() -> None:
             Add a service to the services_offered list.
@@ -19,15 +19,19 @@ class ServicesManager():
 
     def __init__(self, services_offered: list[Service]):
         """Initialize the ServicesManager class.
-        
+
         Arguments:
             services_offered (list): list of services offered by the seller.
         """
         self.services_offered = services_offered
 
+    @property
+    def services_offered(self) -> list[Service]:
+        """Get the services_offered list."""
+        return self._services_offered
+
     def add_service(self) -> None:
-        """Add a service to the services_offered list.
-        """
+        """Add a service to the services_offered list."""
         input_name = input("Agregue el nombre del servicio o producto: ")
         input_price = float(input("Agregue el precio del servicio o producto: "))
         service = Service(input_name, input_price)
@@ -47,7 +51,7 @@ class ServicesManager():
 
     def mod_service(self, name_service: str, to_change: str) -> None:
         """Modify a service from the services_offered list.
-            it could be the name or the price of the service."""
+        it could be the name or the price of the service."""
         for service in self.services_offered:
             if service.name == name_service:
                 if to_change == "name":
