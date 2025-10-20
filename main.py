adfd480a6ee8e8cb9fd71650c52dfec12280dc01
Alejandro Gándara <alejandro.gandara.a@gmail.com>
2025-10-20 13:00:13 -0600

    main.py: main module added

---
+++ main.py
@@ -0,0 +1,10 @@
+"""This is the main module of the application."""
+
+from classes.SalesRecord import SalesRecord
+from classes.Seller import Seller
+from classes.Service import Service
+from classes.ServicesManager import ServicesManager
+
+def main():
+    """Main function to run the application."""
+
