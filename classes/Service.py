ce90997de4feb0fb7039eabb2c81271cc5393250
Alejandro Gándara <alejandro.gandara.a@gmail.com>
2025-10-21 13:23:14 -0600

    Service.py: Bug fix/ getters & setters removed yo avoid recursivity. close #6

--- classes/Service.py
+++ classes/Service.py
@@ -24,34 +24,3 @@ class Service:
         """
         self.name = name
         self.price = price
-
-    @property
-    def name(self) -> str:
-        """The name of the service or product.
-
-        Arguments:
-            name (str): the name of the service or product.
-
-        Returns:
-            str: the name of the service or product.
-        """
-        return self.name
-
-    @name.setter
-    def name(self, name: str) -> None:
-        self.name = name
-
-    @property
-    def price(self) -> float:
-        """The price of the service or product.
-
-        Arguments:
-            price (float): the price of the service or product.
-
-        Returns:
-            float: the price of the service or product."""
-        return self.price
-
-    @price.setter
-    def price(self, price: float) -> None:
-        self.price = price
